fa3e52d33b24104434ca9d7c9c6fdacd47cbcd34
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-07-24 02:02:45 +0100

    feat:done with base navbar design

---
+++ src/lib/component/navbar.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="flex gap-10 rounded-[14px] bg-brand-gray-alt p-4">
+	<Icon icon="heroicons:home" width="24" />
+	<Icon icon="heroicons:folder" width="24" />
+	<Icon icon="heroicons:briefcase" width="24" />
+	<Icon icon="heroicons:wrench-screwdriver" width="24" />
+	<Icon icon="heroicons:pencil-square" width="24" />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Navbar from '$lib/component/navbar.svelte';
+</script>
+
+<section class="h-screen w-full p-10">
+	<nav class="grid place-items-center">
+		<Navbar />
+	</nav>
+</section>
