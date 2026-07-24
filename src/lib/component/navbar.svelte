2bb75abbbe76ffe5fd64bc4f2914cc5d5c681bb5
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-07-24 02:17:34 +0100

    feat:navbar is good to go

--- src/lib/component/navbar.svelte
+++ src/lib/component/navbar.svelte
@@ -1,11 +1,55 @@
-<script>
+<script lang="ts">
 	import Icon from '@iconify/svelte';
+	import { page } from '$app/state';
+
+	const navItems = [
+		{
+			href: '/',
+			icon: 'heroicons:home',
+			label: 'Home'
+		},
+		{
+			href: '/projects',
+			icon: 'heroicons:folder',
+			label: 'Projects'
+		},
+		{
+			href: '/experience',
+			icon: 'heroicons:briefcase',
+			label: 'Experience'
+		},
+		{
+			href: '/tools',
+			icon: 'heroicons:wrench-screwdriver',
+			label: 'Tools'
+		},
+		{
+			href: '/contact',
+			icon: 'heroicons:pencil-square',
+			label: 'Contact'
+		}
+	];
 </script>
 
 <div class="flex gap-10 rounded-[14px] bg-brand-gray-alt p-4">
-	<Icon icon="heroicons:home" width="24" />
-	<Icon icon="heroicons:folder" width="24" />
-	<Icon icon="heroicons:briefcase" width="24" />
-	<Icon icon="heroicons:wrench-screwdriver" width="24" />
-	<Icon icon="heroicons:pencil-square" width="24" />
+	{#each navItems as item}
+		<a
+			href={item.href}
+			class="group relative"
+			// class:text-white={page.url.pathname === item.href}
+			// class:text-gray-500={page.url.pathname !== item.href}
+		>
+			<Icon
+				icon={item.icon}
+				width="24"
+				class="transition-transform duration-300 group-hover:-translate-y-2"
+			/>
+
+			<span
+				class="pointer-events-none absolute top-full left-1/2 mt-4 -translate-x-1/2 translate-y-6 rounded-[14px] bg-brand-gray-alt px-2 py-3 text-sm text-white opacity-0 transition-all duration-400 group-hover:translate-y-0 group-hover:opacity-100"
+			>
+				{item.label}
+			</span>
+		</a>
+	{/each}
 </div>
